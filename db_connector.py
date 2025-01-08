9fd2daa36b134e184030a3a4e9ee6b7c50aab01a
Atharv-raje <84989551+Atharv-raje@users.noreply.github.com>
2025-01-08 17:48:04 -0500

    Add files via upload

---
+++ db_connector.py
@@ -0,0 +1,26 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
+
+def get_db_connection():
+    """
+    Establish a connection to the database and set the search path.
+    """
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT
+    )
+    with conn.cursor() as cursor:
+        cursor.execute("SET search_path TO four_chan_schema, public;")
+    return conn
